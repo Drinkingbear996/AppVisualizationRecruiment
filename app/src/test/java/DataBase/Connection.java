1bed0ff04120ac4ca071b152b89a33b805a8dc2f
Drinkingbear996 <552461073@qq.com>
2023-03-21 09:10:14 +0800

    创建DataBase,View，PythonInternet； 对应数据库，python爬虫，安卓界面

---
+++ app/src/test/java/DataBase/Connection.java
@@ -0,0 +1,12 @@
+package DataBase;
+import java.util.*;
+//举例
+
+public class Connection {
+
+
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+        Connection c=new Connection();
+    }
+}
